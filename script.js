32630978f41a15f6b8725016c8ad569823767269
Sathishexhil <40395456+Sathishexhil@users.noreply.github.com>
2023-09-20 02:30:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ menu.addEventListener('click',function(){
 window.onscroll = () =>{
     navbar.classList.remove('active');
 }
+
+
+let list = document.querySelectorAll('.list');
+
+for(let i=0; i < list.length; i++){
+
+     list[i].addEventListener('click', function(){
+        let j=0;
+
+            while( j < list.length ){
+                list[j++].className = "list";
+            }
+        list[i].className = "list active";
+     })
+}
